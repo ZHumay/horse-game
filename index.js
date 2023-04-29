5447c9965db5b69d46d65b58de24a55a16e8d08d
ZHumay <zehmetihumay@gmail.com>
2023-04-30 01:40:34 +0400

    end

--- index.js
+++ index.js
@@ -1,96 +1,139 @@
-const horse1=document.querySelector(".horse-1")
-const horse2=document.querySelector(".horse-2")
-const horse3=document.querySelector(".horse-3")
-const startbtn=document.querySelector(".start")
-const resetbtn=document.querySelector(".reset")
-
+const horse1 = document.querySelector(".horse-1");
+const horse2 = document.querySelector(".horse-2");
+const horse3 = document.querySelector(".horse-3");
+const startbtn = document.querySelector(".start");
+const resetbtn = document.querySelector(".reset");
+const x = document.getElementById("myAudio");
+let totalAmount = document.querySelector("#total-winnings");
 let moveHorse1IntervalId;
 let moveHorse2IntervalId;
 let moveHorse3IntervalId;
 
-startbtn.addEventListener("click",startRace)
+let userBalance = 0;
+let userBet = 0;
+let selectedHorse = 0;
+let winningHorse = 1;
+const depositBtn = document.querySelector("#deposite-btn");
+const betAmountInput = document.querySelector("#bet-amount");
+const horseSelect = document.querySelector("#horse-select");
+
+depositBtn.addEventListener("click", function () {
+  userBet = Number(betAmountInput.value);
+  selectedHorse = Number(horseSelect.value);
+});
+
+startbtn.addEventListener("click", startRace);
 
 let horse1Position = 0;
 let horse2Position = 0;
-let horse3Position=0
+let horse3Position = 0;
 
 function isRaceRunning() {
   return moveHorse1IntervalId || moveHorse2IntervalId || moveHorse3IntervalId;
 }
 
 function moveHorse1() {
-  if (isRaceRunning){
+  if (isRaceRunning) {
     let randomDistance = Math.floor(Math.random() * 10) + 5;
     horse1Position += randomDistance;
     horse1.style.transform = `translateX(${horse1Position + "px"})`;
     if (horse1Position >= 900) {
       alert("Horse 1 has won the race!");
+
+      winningHorse = 1;
+      if (horseSelect.value === "horse-1" && winningHorse === 1) {
+        userBalance += userBet * 2;
+        totalAmount.textContent = userBalance;
+      }
+
       horse1Position = 0;
       horse2Position = 0;
-      horse3Position=0;
+      horse3Position = 0;
       horse1.style.transform = `translateX(${horse1Position + "px"})`;
       horse2.style.transform = `translateX(${horse2Position + "px"})`;
       horse3.style.transform = `translateX(${horse3Position + "px"})`;
-      isRaceRunning=false;
+      isRaceRunning = false;
+      x.pause();
     }
   }
-
+  
 }
 
 function moveHorse2() {
-  if (isRaceRunning){
+  if (isRaceRunning) {
     let randomDistance = Math.floor(Math.random() * 10) + 5;
     horse2Position += randomDistance;
     horse2.style.transform = `translateX(${horse2Position + "px"})`;
     if (horse2Position >= 900) {
       alert("Horse 2 has won the race!");
+      winningHorse = 2;
+      if (horseSelect.value === "horse-2" && winningHorse === 2) {
+        userBalance += userBet * 2;
+        totalAmount.textContent = userBalance;
+      }
       horse1Position = 0;
       horse2Position = 0;
-      horse3Position=0;
+      horse3Position = 0;
       horse1.style.transform = `translateX(${horse1Position + "px"})`;
       horse2.style.transform = `translateX(${horse2Position + "px"})`;
       horse3.style.transform = `translateX(${horse3Position + "px"})`;
-      isRaceRunning=false;
+      isRaceRunning = false;
+
+      x.pause();
     }
   }
-
 }
 
 function moveHorse3() {
-  if (isRaceRunning){
+  if (isRaceRunning) {
     let randomDistance = Math.floor(Math.random() * 10) + 5;
     horse3Position += randomDistance;
     horse3.style.transform = `translateX(${horse3Position + "px"})`;
     if (horse3Position >= 900) {
       alert("Horse 3 has won the race!");
+      winningHorse = 3;
+      if (horseSelect.value === "horse-3" && winningHorse === 3) {
+        userBalance += userBet * 2;
+        totalAmount.textContent = userBalance;
+      }
+
       horse1Position = 0;
       horse2Position = 0;
-      horse3Position=0;
+      horse3Position = 0;
       horse1.style.transform = `translateX(${horse1Position + "px"})`;
       horse2.style.transform = `translateX(${horse2Position + "px"})`;
       horse3.style.transform = `translateX(${horse3Position + "px"})`;
-      isRaceRunning=false;
+      isRaceRunning = false;
+
+      x.pause();
     }
   }
 }
 
 function startRace(e) {
-  isRaceRunning = true ;
+  isRaceRunning = true;
   moveHorse1IntervalId = setInterval(moveHorse1, 100);
   moveHorse2IntervalId = setInterval(moveHorse2, 100);
-  moveHorse3IntervalId = setInterval(moveHorse3,100);
+  moveHorse3IntervalId = setInterval(moveHorse3, 100);
 }
 
-
-resetbtn.addEventListener("click",reset)
-function reset(e){
-    isRaceRunning = false;
-    horse1Position = 0;
-    horse2Position = 0;
-    horse3Position=0;
-    horse1.style.transform = `translateX(${horse1Position})`;
-    horse2.style.transform = `translateX(${horse2Position})`;
-    horse3.style.transform = `translateX(${horse3Position})`;
+resetbtn.addEventListener("click", reset);
+function reset(e) {
+  isRaceRunning = false;
+  horse1Position = 0;
+  horse2Position = 0;
+  horse3Position = 0;
+  horse1.style.transform = `translateX(${horse1Position})`;
+  horse2.style.transform = `translateX(${horse2Position})`;
+  horse3.style.transform = `translateX(${horse3Position})`;
+  x.pause();
 }
 
-  
+function playAudio() {
+  if (isRaceRunning) {
+    x.autoplay = true;
+  } else {
+    x.pause();
+  }
+  x.load();
+}
